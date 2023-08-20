fade0a9418ca8899d374561be1d52ccb8f227e8a
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2023-08-21 04:15:46 +0900

    깃 캐시 제거

--- API_SERVER/satellite/src/main/java/com/example/satellite/dto/FetchData2ProjectionInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.satellite.dto;
-
-import java.time.LocalDate;
-
-public interface FetchData2ProjectionInterface {
-    LocalDate getFetchDate();
-    Long getFetchCount();
-}
--- API_SERVER/satellite/src/main/java/com/example/satellite/repository/TleDataInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.satellite.repository;
-
-import com.example.satellite.domain.TleDataInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TleDataInfoRepository extends JpaRepository<TleDataInfo, Integer> {
-}
