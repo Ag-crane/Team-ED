737237f31a08aaf0747e4bba6f7fbbbcb636b5f3
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2023-08-21 01:26:53 +0900

    #40 차트 로딩중 스피너 추가

--- WWW/src/assets/js/chart.js
+++ WWW/src/assets/js/chart.js
@@ -1,142 +1,155 @@
-$(function () {
-  /* ChartJS */
-  'use strict'
-  // dashboard
-  if ($('#dashboardChart1').length) {
+function showSpinner () {
+  const spinnerHtml = `
+  <div class="d-flex justify-content-center align-items-center" style="margin-top:100px; height:100px">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>`
+  document.getElementById('spinnerBox').innerHTML = spinnerHtml
+}
 
-    // data fetch
-    async function fetchData () {
-      const response = await fetch(serverUrl+'/tle-data-log/unique-satellite-count')
-      const data = await response.json()
-      return data
-    }
+function hideSpinner () {
+  document.getElementById('spinnerBox').innerHTML = ''
+}
 
-    async function drawChart () {
-      const data = await fetchData()
-      const fetchDate = data.map(d => d.fetchDate)
-      const fetchCount = data.map(d => d.fetchCount)
+// data fetch
+async function fetchData () {
+  const response = await fetch(
+    serverUrl + '/tle-data-log/unique-satellite-count'
+  )
+  const data = await response.json()
+  return data
+}
 
-      // 합계 구하고 출력
-      const sum = fetchCount.reduce((a, b) => a + b, 0)
-      document.getElementById(
-        'dashboardChart-p'
-      ).innerHTML = `최근 7일간 수집한 총 ${sum} 개의 데이터를 수집하였습니다`
-      
-      // 데이터 개수에 따른 y축 최대값 설정
-      const max = Math.max(...fetchCount)
-      const maxRound = Math.ceil(max / 1000) * 1500
+async function drawChart () {
 
-      // 차트
-      var dashboardChart1Canvas = $('#dashboardChart1').get(0).getContext('2d')
-      var dashboardChart1 = new Chart(dashboardChart1Canvas, {
-        type: 'bar',
-        data: {
-          labels: fetchDate,
-          datasets: [
-            {
-              label: 'data',
-              data: fetchCount,
-              backgroundColor: ChartColor[0],
-              borderColor: ChartColor[0],
-              borderWidth: 0
-            }
-          ]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: true,
-          layout: {
-            padding: {
-              left: 0,
-              right: 0,
-              top: 0,
-              bottom: 0
-            }
-          },
-          scales: {
-            xAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                  display: true,
-                  labelString: 'Last 7 days',
-                  fontSize: 15,
-                  lineHeight: 2
-                },
-                ticks: {
-                  fontColor: ChartColor[7],
-                  stepSize: 50,
-                  min: 0,
-                  max: 150,
-                  autoSkip: true,
-                  autoSkipPadding: 15,
-                  maxRotation: 0,
-                  maxTicksLimit: 10
-                },
-                gridLines: {
-                  display: false,
-                  drawBorder: false,
-                  color: 'transparent',
-                  zeroLineColor: '#eeeeee'
-                }
-              }
-            ],
-            yAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                  display: true,
-                  labelString: 'Amount of Collected Data',
-                  fontSize: 15,
-                  lineHeight: 2
-                },
-                ticks: {
-                  display: true,
-                  autoSkip: false,
-                  maxRotation: 0,
-                  fontColor: '#bfccda',
-                  stepSize: 2000,
-                  min: 0,
-                  max: maxRound,
-                },
-                gridLines: {
-                  drawBorder: false
-                }
-              }
-            ]
-          },
-          legend: {
-            display: false
-          },
-          legendCallback: function (chart) {
-            var text = []
-            text.push('<div class="chartjs-legend"><ul>')
-            for (var i = 0; i < chart.data.datasets.length; i++) {
-              console.log(chart.data.datasets[i]) // see what's inside the obj.
-              text.push('<li>')
-              text.push(
-                '<span style="background-color:' +
-                  chart.data.datasets[i].backgroundColor +
-                  '">' +
-                  '</span>'
-              )
-              text.push(chart.data.datasets[i].label)
-              text.push('</li>')
-            }
-            text.push('</ul></div>')
-            return text.join('')
-          },
-          elements: {
-            point: {
-              radius: 0
+  showSpinner()
+  const data = await fetchData()
+  hideSpinner()
+  document.getElementById('dashboardChart1').style.height = '250px'
+
+  const fetchDate = data.map(d => d.fetchDate)
+  const fetchCount = data.map(d => d.fetchCount)
+
+  // 합계 구하고 출력
+  const sum = fetchCount.reduce((a, b) => a + b, 0)
+  document.getElementById(
+    'dashboardChart-p'
+  ).innerHTML = `최근 7일간 수집한 총 ${sum} 개의 데이터를 수집하였습니다`
+
+  // 데이터 개수에 따른 y축 최대값 설정
+  const max = Math.max(...fetchCount)
+  const maxRound = Math.ceil(max / 1000) * 1500
+
+  // 차트
+  var dashboardChart1Canvas = $('#dashboardChart1').get(0).getContext('2d')
+  var dashboardChart1 = new Chart(dashboardChart1Canvas, {
+    type: 'bar',
+    data: {
+      labels: fetchDate,
+      datasets: [
+        {
+          label: 'data',
+          data: fetchCount,
+          backgroundColor: ChartColor[0],
+          borderColor: ChartColor[0],
+          borderWidth: 0
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0
+        }
+      },
+      scales: {
+        xAxes: [
+          {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Last 7 days',
+              fontSize: 15,
+              lineHeight: 2
+            },
+            ticks: {
+              fontColor: ChartColor[7],
+              stepSize: 50,
+              min: 0,
+              max: 150,
+              autoSkip: true,
+              autoSkipPadding: 15,
+              maxRotation: 0,
+              maxTicksLimit: 10
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+              color: 'transparent',
+              zeroLineColor: '#eeeeee'
             }
           }
+        ],
+        yAxes: [
+          {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Amount of Collected Data',
+              fontSize: 15,
+              lineHeight: 2
+            },
+            ticks: {
+              display: true,
+              autoSkip: false,
+              maxRotation: 0,
+              fontColor: '#bfccda',
+              stepSize: 2000,
+              min: 0,
+              max: maxRound
+            },
+            gridLines: {
+              drawBorder: false
+            }
+          }
+        ]
+      },
+      legend: {
+        display: false
+      },
+      legendCallback: function (chart) {
+        var text = []
+        text.push('<div class="chartjs-legend"><ul>')
+        for (var i = 0; i < chart.data.datasets.length; i++) {
+          console.log(chart.data.datasets[i]) // see what's inside the obj.
+          text.push('<li>')
+          text.push(
+            '<span style="background-color:' +
+              chart.data.datasets[i].backgroundColor +
+              '">' +
+              '</span>'
+          )
+          text.push(chart.data.datasets[i].label)
+          text.push('</li>')
         }
-      })
-      document.getElementById('bar-traffic-legend').innerHTML =
-        dashboardChart1.generateLegend()
+        text.push('</ul></div>')
+        return text.join('')
+      },
+      elements: {
+        point: {
+          radius: 0
+        }
+      }
     }
+  })
+  document.getElementById('bar-traffic-legend').innerHTML =
+    dashboardChart1.generateLegend()
+}
 
-    drawChart();
-  }
-})
+drawChart()
